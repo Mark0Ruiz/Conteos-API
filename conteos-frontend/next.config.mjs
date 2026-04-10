ea78603305556215962b1529dc98a638094616f4
Mark0Ruiz <marco.ruiz.94.01@gmail.com>
2026-04-10 12:47:41 -0600

    Remove eslint ignoreDuringBuilds config and package-lock.json

--- conteos-frontend/next.config.mjs
+++ conteos-frontend/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // ⚠ Permite que el build termine aunque haya errores de ESLint
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // ⚠ Permite que el build termine aunque haya errores de TypeScript
     ignoreBuildErrors: true,
