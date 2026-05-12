b6fcf177477a336ed872ac40668533ab3053cab3
Mark0Ruiz <marco.ruiz.94.01@gmail.com>
2026-05-12 12:44:35 -0600

    fix: importar settings en main para CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import conteos, auth, catalogo
+from app.core.config import settings
 from app.core.database import engine
 from app.models import models
 
